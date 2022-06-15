44495af7810b8b3d7270039fd476708b17a8c6d9
twister-dev <twister-cash@protonmail.com>
2022-06-14 22:08:16 -0700

    fix: exports

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = {
+    // incremental tree
     calculateSubtrees: require('./src/calculateSubtrees'),
+    calculateSubtreesV2: require('./src/calculateSubtreesV2'),
+
+    // mimc version
     calculateUpdateProof: require('./src/calculateUpdateProof'),
     calculateMassUpdateProof: require('./src/calculateMassUpdateProof'),
+    verifyProof: require('./src/verifyProof'),
+
+    // hashers & utils
     mimcSponge: require('./src/mimcSponge'),
     poseidon: require('./src/poseidon'),
     utils: require('./src/utils'),
-    verifyProof: require('./src/verifyProof'),
 }
