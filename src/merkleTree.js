580e27bdac9d64b3caa37d80f56acef924126ec1
twister-dev <twister-cash@protonmail.com>
2022-09-08 04:29:57 -0700

    fix baseString in tree

--- src/merkleTree.js
+++ src/merkleTree.js
@@ -1,11 +1,11 @@
 const { MerkleTree: FixedMerkleTree } = require("fixed-merkle-tree");
-const { zero } = require('./utils');
+const { getZero } = require('./utils');
 
 module.exports = class MerkleTree extends FixedMerkleTree {
-    constructor({ hasher, levels = 20, leaves = [] }) {
+    constructor({ hasher, levels = 20, leaves = [], baseString = "empty" }) {
         super(levels, leaves, {
             hashFunction: (left, right) => hasher([left, right]),
-            zeroElement: zero(),
+            zeroElement: getZero(baseString),
         });
     };
 };
